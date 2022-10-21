40679a17d57fe9443f1a507c7b2924a68f0b7324
AlejandroP <leho.patino@gmail.com>
2022-10-21 00:24:27 -0500

    code

--- user.php
+++ user.php
@@ -1,31 +1,29 @@
 <?php
-if( isset( $_POST['submit_form'] ) )
-{
- validate_data($data)
- {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = strip_tags($data);
-  $data = htmlspecialchars($data);
-  $data = mysqli_real_escape_string($data);
-  return $data;    
- }
+    $cedula = $_POST['cedula'];
+    $nombre = $_POST['nombre'];
+    $pass = $_POST['pass'];
+    $email = $_POST['email'];
+    $type = $_POST['type'];
+    $comment = $_POST['comment'];
 
- $cedula = validate_data( $_POST['cedula'] );
- $nombre = validate_data( $_POST['nombre'] );
- $pass = validate_data( $_POST['pass'] );
- $email = validate_data( $_POST['email'] );
- $type = validate_data( $_POST['type'] );
- $comment = validate_data( $_POST['comment'] );
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '';
 
- $host = 'localhost';
- $user = 'root';
- $pass = ' ';
+    $mysqli = new mysqli($host, $user, $pass, 'usuario');
 
- mysql_connect($host, $user, $pass);
- mysql_select_db('usuario');
+    // Prueba de coneccion
+    if ($mysqli -> connect_errno) {
+        echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+        exit();
+    }
 
- $insertdata=" INSERT INTO usuarios VALUES( '$cedula','$nombre','$pass', '$email', $type, $comment ) ";
- mysqli_query($insertdata);
-}
+    $insertdata=" INSERT INTO usuarios(Cedula, Nombre, Password, Correo, tipo, Comentarios) VALUES( '$cedula','$nombre','$pass','$email','$type','$comment' ) ";
+        // Perform query
+    if ($result = $mysqli -> query($insertdata)) {
+        echo "Datos Guardados con exito";
+    }
+
+    
+    $mysqli -> close();
 ?>
